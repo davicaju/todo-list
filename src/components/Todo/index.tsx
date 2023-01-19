a3e0dd017a4622b5888e24714c8e9163a9fee856
Davi Caju <devcaju@gmail.com>
2023-01-19 20:16:37 -0300

    feat: save tasks in localStorage

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -1,22 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+interface Task {
+  id: number;
+  title: string;
+}
 
 export function Todo() {
-  const databaseTasks = [
-    {
-      id: 0,
-      title: "Watch a movie on English",
-    },
-    {
-      id: 1,
-      title: "Learn Clean Code",
-    },
-    {
-      id: 2,
-      title: "Make a coffee",
-    },
-  ];
-
-  const [tasks, setTasks] = useState(databaseTasks);
+  const [tasks, setTasks] = useState<Task[]>([]);
   const [value, setValue] = useState("");
 
   function handleCreateTask() {
@@ -31,12 +21,32 @@ export function Todo() {
       title: value,
     };
 
-    setTasks((oldState) => [...oldState, newTask]);
+    const updateTasks = [...tasks, newTask];
+
+    setTasks(updateTasks);
+
+    localStorage.setItem("tasks", JSON.stringify(updateTasks));
   }
 
   function handleDeleteTask(id: number) {
-    setTasks(tasks.filter((task) => id !== task.id));
+    const filteredTasks = tasks.filter((task) => id !== task.id);
+
+    setTasks(filteredTasks);
+
+    localStorage.setItem("tasks", JSON.stringify(filteredTasks));
   }
+
+  useEffect(() => {
+    function loadTasksFromLocalStorage() {
+      const localStorageTasks = localStorage.getItem("tasks");
+
+      if (localStorageTasks) {
+        setTasks(JSON.parse(localStorageTasks));
+      }
+    }
+
+    loadTasksFromLocalStorage();
+  }, []);
 
   return (
     <div>
