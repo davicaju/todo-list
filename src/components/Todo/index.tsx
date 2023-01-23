8f8cde61c10c681bb186365b51c45166327a58af
Davi Caju <devcaju@gmail.com>
2023-01-23 20:58:11 -0300

    feat: done task function

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -4,11 +4,24 @@ import { Header } from "../Header";
 interface Task {
   id: number;
   title: string;
+  isCompleted: boolean;
 }
 
 export function Todo() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [value, setValue] = useState("");
+
+  useEffect(() => {
+    function loadTasksFromLocalStorage() {
+      const localStorageTasks = localStorage.getItem("tasks");
+
+      if (localStorageTasks) {
+        setTasks(JSON.parse(localStorageTasks));
+      }
+    }
+
+    loadTasksFromLocalStorage();
+  }, []);
 
   function handleCreateTask() {
     if (!value) {
@@ -20,6 +33,7 @@ export function Todo() {
     const newTask = {
       id: tasks.length + 1,
       title: value,
+      isCompleted: false,
     };
 
     const updateTasks = [...tasks, newTask];
@@ -37,17 +51,15 @@ export function Todo() {
     localStorage.setItem("tasks", JSON.stringify(filteredTasks));
   }
 
-  useEffect(() => {
-    function loadTasksFromLocalStorage() {
-      const localStorageTasks = localStorage.getItem("tasks");
+  function handleCompleteTask(id: number) {
+    const filteredTasks = tasks.map((task) =>
+      task.id === id ? { ...task, isCompleted: !task.isCompleted } : task
+    );
 
-      if (localStorageTasks) {
-        setTasks(JSON.parse(localStorageTasks));
-      }
-    }
+    setTasks(filteredTasks);
 
-    loadTasksFromLocalStorage();
-  }, []);
+    localStorage.setItem("tasks", JSON.stringify(filteredTasks));
+  }
 
   return (
     <div>
@@ -64,7 +76,19 @@ export function Todo() {
         <ul>
           {tasks.map((task) => (
             <li key={task.id}>
-              {task.title}{" "}
+              <input
+                type="checkbox"
+                checked={task.isCompleted}
+                onChange={() => handleCompleteTask(task.id)}
+              />
+
+              <p
+                style={
+                  task.isCompleted ? { textDecoration: "line-through" } : {}
+                }
+              >
+                {task.title}
+              </p>
               <button onClick={() => handleDeleteTask(task.id)}>remove</button>
             </li>
           ))}
